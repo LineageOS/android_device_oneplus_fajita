fdb257228dcdbe7aae687efe67858ce4ecce97a9
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-09-09 16:52:33 +0200

    fajita-common: Move soong namespaces to common.mk
    * This is a product property
    
    Change-Id: I411428687839fca7c794d488bb48e5f1773e570b

--- device.mk
+++ device.mk
@@ -47,6 +47,10 @@ PRODUCT_PACKAGES += \
     vendor.oneplus.fingerprint.extension@1.0 \
     vendor.oneplus.hardware.display@1.0
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    device/oneplus/common
+
 # Inherit from oneplus sdm845-common
 $(call inherit-product, device/oneplus/sdm845-common/common.mk)
 
