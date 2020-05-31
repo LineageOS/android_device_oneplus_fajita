b965f23eba09947a7b58c39cb3930cbbd6f3908a
LuK1337 <priv.luk@gmail.com>
2020-05-31 21:52:00 +0200

    fajita: Update libfod extension function prototypes
    Change-Id: I5c43c71326caf21a85863b74ed0a893057d1cfa8

--- fod/FodExtension.cpp
+++ fod/FodExtension.cpp
@@ -20,6 +20,6 @@ uint32_t getFodZOrder(uint32_t, bool touched) {
     return touched ? 0xfc8 : 0xbd6;
 }
 
-uint32_t getFodUsageBits(uint32_t usageBits, bool) {
+uint64_t getFodUsageBits(uint64_t usageBits, bool) {
     return usageBits;
 }
