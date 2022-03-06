8e570a4e0ede2acf26d17ae045349ce64aca5cd4
Edwin Moquete <edwinmmoquete@gmail.com>
2022-03-05 21:06:21 -0400

    fajita: Build audio amplifier HAL
    Change-Id: Ib6bb31397bccf90d449f5be3ff0e84a2c9f87a19

--- device.mk
+++ device.mk
@@ -41,6 +41,10 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 
+# Audio
+PRODUCT_PACKAGES += \
+    audio_amplifier.sdm845
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.oneplus \
