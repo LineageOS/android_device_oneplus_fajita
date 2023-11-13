775da7e50ecc6cd9157b6394c47ade301114d8be
Edwin Moquete <edwinmmoquete@gmail.com>
2023-11-13 19:55:48 +0000

    fajita: Add soong namespace
    To avoid breaking build when device trees for both
    enchilada and fajita are synced
    
    Change-Id: I7b75b90e3ef3072f3af6218b0103ba5e9cf725cd

--- device.mk
+++ device.mk
@@ -47,6 +47,10 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.oneplus \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # WiFi
 PRODUCT_PACKAGES += \
     TargetWifiOverlay
